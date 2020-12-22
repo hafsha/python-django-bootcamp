28eeaf6a8e526164fdf073d2ea64d6711dcff1fb
hafsha <hafshamirpur@gmail.com>
2020-12-22 13:12:58 +0600

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,9 @@
+#mystuff = {"key1":"value",'key2':'value2','key3':{'123':[1,2,'grab me']}}
+#print(mystuff["key1"])
+#print(mystuff['key2'])
+#print(mystuff['key3']['123'][2].upper())
+
+mystuff = {'lunch':'pizza','bfast':'eggs'}
+mystuff['lunch'] = 'burger'
+mystuff['dinner'] = 'pasta'
+print(mystuff)
---
+++ strings.py
@@ -0,0 +1,4 @@
+mystring = 'abcdefg'
+#print(mystring)
+#print(mystring[0])
+print(mystring[-1])
